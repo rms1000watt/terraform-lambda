a06dc6c11053df8477a3f82aebef21ad5e1cde08
Ryan Smith <rms1000watt@gmail.com>
2018-01-22 09:23:52 -0800

    WIP permissions issue

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_lambda_permission" "allow_api_gateway" {
   statement_id  = "AllowExecutionFromApiGateway"
   action        = "lambda:InvokeFunction"
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.example_api.id}/*/${aws_api_gateway_method.example_api_method.http_method}${aws_api_gateway_resource.example_api_resource.path_part}"
+  source_arn    = "arn:aws:execute-api:${var.region}:${var.account_id}:${aws_api_gateway_rest_api.example_api.id}/*/${aws_api_gateway_method.example_api_method.http_method}/${aws_api_gateway_resource.example_api_resource.path_part}"
 }
 
 resource "aws_api_gateway_deployment" "example_deployment_dev" {
